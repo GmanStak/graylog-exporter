7d2a0a5dfe33e6643fcf43ecfb6e7bac29b63065
GmanStak <836375453@qq.com>
2023-11-08 15:08:27 +0800

    Update graylog-exporter.py

--- graylog-exporter.py
+++ graylog-exporter.py
@@ -30,6 +30,22 @@ def get_Graylog():
         graylog_index_size_bytes.labels(id=index_id,index_prefix=index_prefix).set(size)
     sidecar_num = graylog.get_sidecar_num()
     graylog_sidecar_count_num.labels(host=graylog_server).set(sidecar_num)
+        sidecar_info_list = graylog.get_sidecar_node_status()
+    for sidecar_info in sidecar_info_list:
+        sidecar_node_name = sidecar_info['node_name']
+        sidecar_node_system = sidecar_info['node_details']['operating_system']
+        sidecar_node_ip = sidecar_info['node_details']['ip']
+        collector_status = sidecar_info['node_details']['status']['collectors']
+        if not collector_status:
+           sidecar_node_collector_status = "None"
+           graylog_sidecar_node_status.labels(sidecar_node_name=sidecar_node_name,sidecar_node_system=sidecar_node_system,sidecar_node_ip=sidecar_node_ip,sidecar_node_collect_status=sidecar_node_collector_status).set(0)
+        else:
+           sidecar_node_collector_status = collector_status[0]['message']
+           if sidecar_node_collector_status == 'Running':
+              graylog_sidecar_node_status.labels(sidecar_node_name=sidecar_node_name,sidecar_node_system=sidecar_node_system,sidecar_node_ip=sidecar_node_ip,sidecar_node_collect_status=sidecar_node_collector_status).set(1)
+           else:
+              graylog_sidecar_node_status.labels(sidecar_node_name=sidecar_node_name,sidecar_node_system=sidecar_node_system,sidecar_node_ip=sidecar_node_ip,sidecar_node_collect_status=sidecar_node_collector_status).set(0)
+    graylog.logout()
     return Response(prometheus_client.generate_latest(REGISTRY), mimetype="text/plain")
 
 if __name__ == '__main__':
