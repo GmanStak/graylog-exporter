223e5bba23c23d5e7f3e9183e334e514486e5ae4
GmanStak <836375453@qq.com>
2023-11-08 15:04:23 +0800

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -7,3 +7,4 @@ graylog_index_count_num = Gauge("graylog_index_count_num", "graylog索引数量"
 graylog_index_size_bytes = Gauge("graylog_index_size_bytes", "graylog 索引容量", ['id','index_prefix'], registry=REGISTRY)
 graylog_sidecar_count_num = Gauge("graylog_sidecar_count_num", "graylog sidecar 数量",['host'], registry=REGISTRY)
 graylog_cluster_info = Gauge("graylog_cluster_info","graylog 集群参数", ['id','index_prefix'], registry=REGISTRY)
+graylog_sidecar_node_status = Gauge("sidecar_node_status","graylog sidecar 节点运行状态",['sidecar_node_name','sidecar_node_system','sidecar_node_ip','sidecar_node_collect_status'],registry=REGISTRY)
